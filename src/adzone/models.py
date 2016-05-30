cdc411b4173ddf2a18504d3216991c2ecadd3513
Charlie DeTar <cfd@media.mit.edu>
2016-05-30 15:27:17 -0600

    Remove reference to django.contrib.auth.User

--- src/adzone/models.py
+++ src/adzone/models.py
@@ -8,7 +8,6 @@
 import datetime
 
 from django.db import models
-from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from django.utils.translation import ugettext_lazy as _
 
